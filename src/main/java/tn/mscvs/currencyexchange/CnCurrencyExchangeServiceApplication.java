7404c1524becd30b2f2ef5c6294179aeb773495e
cnoriega20 <c79.noriega@gmail.com>
2020-10-15 10:20:12 -0500

    Initial commit: Spring initializr project structure.

---
+++ src/main/java/tn/mscvs/currencyexchange/CnCurrencyExchangeServiceApplication.java
@@ -0,0 +1,13 @@
+package tn.mscvs.currencyexchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CnCurrencyExchangeServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CnCurrencyExchangeServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tn/mscvs/currencyexchange/CnCurrencyExchangeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package tn.mscvs.currencyexchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CnCurrencyExchangeServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
