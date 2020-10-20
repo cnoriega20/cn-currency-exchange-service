cb801d2faa53eb44a42f1763a51df3ba206a0515
cnoriega20 <c79.noriega@gmail.com>
2020-10-20 02:33:14 -0500

    Currency Exchange service registered to Eureka Naming Server.

--- src/main/java/tn/mscvs/currencyexchange/CnCurrencyExchangeServiceApplication.java
+++ src/main/java/tn/mscvs/currencyexchange/CnCurrencyExchangeServiceApplication.java
@@ -2,8 +2,10 @@ package tn.mscvs.currencyexchange;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class CnCurrencyExchangeServiceApplication {
 
     public static void main(String[] args) {
