9d84b2993a0987a1f4a42cbc1f769c8f5e062116
cnoriega20 <c79.noriega@gmail.com>
2020-10-25 00:04:54 -0500

    Setting up Spring Cloud Sleuth.

--- src/main/java/tn/mscvs/currencyexchange/CnCurrencyExchangeServiceApplication.java
+++ src/main/java/tn/mscvs/currencyexchange/CnCurrencyExchangeServiceApplication.java
@@ -1,8 +1,10 @@
 package tn.mscvs.currencyexchange;
 
+import brave.sampler.Sampler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @EnableDiscoveryClient
@@ -10,6 +12,11 @@ public class CnCurrencyExchangeServiceApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(CnCurrencyExchangeServiceApplication.class, args);
+    }
+
+    @Bean
+    public Sampler defaultSampler(){
+        return Sampler.ALWAYS_SAMPLE;
     }
 
 }
